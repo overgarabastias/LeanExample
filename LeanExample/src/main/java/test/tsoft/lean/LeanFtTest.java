bf8692ff97c6a6f8f8abd9b9ffb70466ea5ef07c
omar.vergara <omar.vergara@CL-NB-OMVE.tsoftglobal.com>
2017-05-31 15:41:18 -0400

    se carga proyesto

---
+++ LeanExample/src/main/java/test/tsoft/lean/LeanFtTest.java
@@ -0,0 +1,46 @@
+package test.tsoft.lean;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.hp.lft.sdk.*;
+import com.hp.lft.sdk.web.Browser;
+import com.hp.lft.sdk.web.BrowserFactory;
+import com.hp.lft.sdk.web.BrowserType;
+import unittesting.*;
+
+public class LeanFtTest extends UnitTestClassBase {
+
+	public LeanFtTest() {
+		//Change this constructor to private if you supply your own public constructor
+	}
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		instance = new LeanFtTest();
+		globalSetup(LeanFtTest.class);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		globalTearDown();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() throws GeneralLeanFtException {
+		Browser browser = BrowserFactory.launch(BrowserType.INTERNET_EXPLORER);
+		browser.navigate("www.falabella.cl");
+	}
+
+}
+ 
